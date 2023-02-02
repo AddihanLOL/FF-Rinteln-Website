ef11d06bceb3d14a320c11aeaef0336325fe08fd
Addihanlol <Addihanlol@protonmail.com>
2023-02-02 10:21:48 +0100

    first deploy

---
+++ pages/components/NavBar.js
@@ -0,0 +1,33 @@
+import styles from "./navbar.module.css"
+
+import Link from "next/link"
+
+
+export default function NavBar(props){
+
+    const jfClick = props["jf"];
+    const joinClick = props["join"];
+    const contactClick = props["contact"];
+
+    return (
+        <>
+        <nav className={styles.navbar}>
+            <Link href={"https://www.rinteln.de/"}>
+                <img src="./logoRinteln.png" className={styles.logo} width={60} height={60}></img>
+            </Link>
+            <ul className={styles.navLinks}>
+                <li>
+                    <button id="scrollJF" onClick={jfClick}>Jugend-Feuerwehr</button>
+                </li>
+                <li>
+                    <button id="scrollJoin" onClick={joinClick}>Mitgleid werden</button>
+                </li>
+                <li>
+                    <button id="scrollContact" onClick={contactClick}>Kontakt</button>
+                </li>
+            </ul>
+        </nav>
+        </>
+
+    )
+}
