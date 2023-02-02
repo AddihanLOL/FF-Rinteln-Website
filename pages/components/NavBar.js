4b14d1500dae9e83298aede2405d132f755c33d5
Addihanlol <Addihanlol@protonmail.com>
2023-02-03 00:22:10 +0100

    update

--- pages/components/NavBar.js
+++ pages/components/NavBar.js
@@ -1,5 +1,5 @@
-import styles from "./navbar.module.css"
-
+import "@/styles/navbar.module.css"
+import styles from "@/styles/navbar.module.css"
 import Link from "next/link"
 
 
@@ -8,14 +8,20 @@ export default function NavBar(props){
     const jfClick = props["jf"];
     const joinClick = props["join"];
     const contactClick = props["contact"];
+    const newsClick = props["news"];
 
     return (
         <>
+        
         <nav className={styles.navbar}>
             <Link href={"https://www.rinteln.de/"}>
-                <img src="./logoRinteln.png" className={styles.logo} width={60} height={60}></img>
+                <img src={"/logoRinteln.png"} className={styles.logo} width={60} height={60}></img>
             </Link>
+            
             <ul className={styles.navLinks}>
+                <li>
+                    <button id="scrollNews" onClick={newsClick}>Aktuelles</button>
+                </li>
                 <li>
                     <button id="scrollJF" onClick={jfClick}>Jugend-Feuerwehr</button>
                 </li>
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,18 @@
 import Head from "next/head"
-import styles from "./Home.module.css"
+import "@/styles/index.module.css"
+import styles from "@/styles/index.module.css"
 import React, { useRef } from 'react';
-import NavBar from "./components/NavBar"
+import NavBar from "./components/NavBar.js"
+
+import News from "@/public/news.json"
 
 
-export default function Home() {
-    
+
+export default function Home() {    
     const jfDiv = useRef(null);
     const joinDiv = useRef(null);
     const contactDiv = useRef(null);
+    const newsDiv = useRef(null);
 
     const jfClick = () => {
         jfDiv.current.scrollIntoView({ behavior: 'smooth' });
@@ -19,32 +23,54 @@ export default function Home() {
     const contactClick = () => {
         contactDiv.current.scrollIntoView({ behavior: 'smooth' });
     };
+    const newsClick = () => {
+        newsDiv.current.scrollIntoView({ behavior: 'smooth' });
+    };
+
 
     return (
         <>
         <Head>
-            <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Head>
         
-        <div className={styles.container}>
-            
-            <div className={styles.introduction} id="intruduction">
-                <NavBar jf={jfClick} join={joinClick} contact={contactClick}></NavBar>
-                <h1>Einleitung</h1>
-            </div>
+        <div className={styles.header}>
+            <img src={"./background.png"} className={styles.thumbnail} />
+            <span className={styles.titel}>
+                Freiwillige Feuerwehr Rinteln
+            </span>
 
+        </div>
+        <div className={styles.container}>
+            <NavBar jf={jfClick} join={joinClick} contact={contactClick} news={newsClick}></NavBar>
+            
+            
+            <div className={styles.news} ref={newsDiv}>
+                <h1>Aktuelles</h1>
+                <div className={styles.card}>
+                    <h2>{News["title"]}</h2>
+                    <span>{News["value"]}</span>
+                    <h5>{News["date"]}</h5>
+                </div>
+
+                <img src={"./images/315981355_508876777950579_1750996421873977670_n.jpg"} className={styles.newsImageBottom} />
+                <img src={"./images/318723065_1879910239012519_1063188433890244806_n.jpg"} className={styles.newsImageRight}/>
+            </div>
             <div className={styles.jf} ref={jfDiv}>
-                <h1>JugendFeuerwehr</h1>
+                <h1>Kinder und Jugendfeuerwehr</h1>
             </div>
             <div className={styles.join} ref={joinDiv}>
-                <h1>Mitlgied werden</h1>
+                <h1>Mitglied werden</h1>
             </div>
             <div className={styles.contact} ref={contactDiv}>
                 <h1>Kontakt</h1>
             </div>
-
+            <div className={styles.impressum} id="impressum">
+                <h1>impressum</h1>
+            </div>
         </div>
         </>
+        
     )
   }
   
